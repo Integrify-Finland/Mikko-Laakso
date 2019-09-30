bde0adb25bc6c6e3dc9b7160db7d2ca6c75f6426
tripheo0412 <tripheo0412@gmail.com>
2019-09-30 18:48:59 +0300

    testing failed

--- src/components/Header/tests/Header.test.tsx
+++ src/components/Header/tests/Header.test.tsx
@@ -5,7 +5,7 @@ import {render} from '@testing-library/react'
 describe('<Header/>', () => {
 	it('should render Header with <TESTING> as title', () => {
 		// Arrange
-		const {getByText} = render(<Header siteTitle={'TESTING'} />)
+		const {getByText} = render(<Header siteTitle={'TESTiNG'} />)
 
 		// Act
 
