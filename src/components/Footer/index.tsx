2b4c6b9a2bd95c60b3225ffa424cf6d9c20269c4
yazanAlhalabi <yazanalhalabi1@gmail.com>
2019-11-08 17:22:58 +0200

    overall improvments

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,70 @@
 import React from 'react'
-
-import './styles.scss'
+import FacebookIcon from '../SocialMediaIcons/Facebook/index'
+import TwitterIcon from '../SocialMediaIcons/Twitter/index'
+import InstagramIcon from '../SocialMediaIcons/Instagram/index'
 const kokoomusLogo = require('./assets/kokoomus-logo.svg')
 
+import './styles.scss'
+
 const Footer: React.FC = () => {
+	const year = new Date().getFullYear()
+
+	const copyright = `Copyright ${String.fromCharCode(169)} ${year} Mikko Laakso`
 	return (
 		<div className="footer">
+			<div className="footer__left-box">
+				<div className="footer__social-media">
+					<a
+						href="https://www.facebook.com/janneheikkinenpage/"
+						target="blank"
+						rel="noopener"
+						title="Go to Facebook"
+					>
+						<FacebookIcon iconSize="big" />
+					</a>
+
+					<a
+						href="https://twitter.com/heikkinenjanne"
+						target="blank"
+						rel="noopener"
+						title="Go to Twitter"
+					>
+						<TwitterIcon iconSize="big" />
+					</a>
+
+					<a
+						href="https://www.instagram.com/janneheikkinen/"
+						target="blank"
+						rel="noopener"
+						title="Go to Instagram"
+					>
+						<InstagramIcon iconSize="big" />
+					</a>
+				</div>
+
+				<div className="footer__contact-container">
+					<div className="footer__contact1">
+						<h1>Mikko Laakso</h1>
+						<p>
+							Kauppatieteiden maisteri, Kaupunginvaltuutettu, Espoo
+							<br />
+							Kotipaikka:
+							<br />
+							Olari, Espoo
+							<br />
+							Puhelin:
+							<br />
+							+358 {'('}0{')'} 50 3839432
+							<br />
+							Sähköposti:
+							<br />
+							mikko.laakso(at)alumni.aalto.fi
+						</p>
+					</div>
+				</div>
+
+				<p className="footer__copyright">{copyright}</p>
+			</div>
 			<img src={kokoomusLogo} alt="Kokoomus Logo" />
 		</div>
 	)
