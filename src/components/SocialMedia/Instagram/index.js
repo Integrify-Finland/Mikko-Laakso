fddb8a36db497cef39e65579ef8b77bb025ecc83
SexyBeast0412 <tripheo0412@gmail.com>
2019-12-05 17:48:08 +0200

    changes to client feeback and add instagram

--- src/components/SocialMedia/Instagram/index.js
+++ src/components/SocialMedia/Instagram/index.js
@@ -35,11 +35,11 @@ class Instagram extends Component {
         <h3 className="instagram-column__title">Instagram</h3>
         <div style={{ margin: 'auto', display: 'block', overflow: 'hidden' }}>
           <iframe
-            title="Janne's Instagram feed"
-            src="https://cdn.lightwidget.com/widgets/b697a021935e50f8b4656ecedebb7698.html"
+            title="Mikko's Instagram feed"
+            src="//lightwidget.com/widgets/52d1008562005062b1a0fe86ea50407b.html"
             scrolling="no"
             allowtransparency="true"
-            className="lightwidget-widget"
+            class="lightwidget-widget"
             style={{ width: '100%', border: '0', height: '580px' }}
           ></iframe>
         </div>
